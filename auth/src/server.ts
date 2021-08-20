ee4059670f141cf8a143b4ba2862b36f4073b181
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-20 10:18:13 +0700

    Skaffold setup done

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,7 +1,7 @@
-import express  from "express";
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.listen(5000, () => {
-     console.log('App is running on port 5000')
-})
+  console.log('App is running on port 5000 !!!!');
+});
