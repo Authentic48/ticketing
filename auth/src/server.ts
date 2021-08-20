df92587826ce2198ad7a5c93df164afcb4f7f99e
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-20 11:01:37 +0700

    ingress setup done

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/api/auth/login', (req, res) => {
+  res.json("It's working !!!");
+});
+
 app.listen(5000, () => {
   console.log('App is running on port 5000 !!!!');
 });
