4e63d941dfd4603d23c39ea86c22b2d1cd6af59e
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-09-02 09:03:36 +0700

    current-user unit testing done

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+describe('GET /api/users/currentuser ', () => {
+  it('responds with details about the current user', async () => {
+    const authResponse = await request(app)
+      .post('/api/users/register')
+      .send({
+        name: 'test',
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(201);
+
+    const cookie = authResponse.get('Set-Cookie');
+
+    const response = await request(app)
+      .get('/api/users/currentuser')
+      .set('Cookie', cookie)
+      .send({})
+      .expect(200);
+
+    expect(response.body.currentUser.email).toEqual('test@test.com');
+  });
+
+  it('responds with null if not authenticated', async () => {
+    const response = await request(app)
+      .get('/api/users/currentuser')
+      .send({})
+      .expect(200);
+
+    expect(response.body.currentUser).toEqual(null);
+  });
+});
