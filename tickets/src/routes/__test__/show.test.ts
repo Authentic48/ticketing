385284edb24ef55115f4d7dd61ee7d82fc24c328
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-09-03 07:45:39 +0700

    unit testing Error fixed

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,11 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 describe('GET/api/tickets:id', () => {
   it('returns a 404 if the ticket is not found', async () => {
-    await request(app).get('/api/tickets/bhsyvaddan').send().expect(404);
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app).get(`/api/tickets/${id}`).send().expect(404);
   });
 
   it("returns the ticket if it's found ", async () => {
