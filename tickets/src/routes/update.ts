c598e8d1e28cacb66145049113737b09631ca0ab
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-05 09:03:48 +0000

    ticket created and updated publisher done

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotFoundError,
 } from '@authentic48/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../config/nats-wrapper';
 
 const route = express.Router();
 
@@ -39,6 +41,13 @@ route.put(
     });
 
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     return res.send(ticket);
   }
 );
