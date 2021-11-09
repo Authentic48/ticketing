00e8383ebf71f7060d30898d072a15dc53eb202c
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-11-09 03:26:18 +0000

    expiration basic setup done

--- expiration/src/server.ts
+++ expiration/src/server.ts
@@ -1,20 +1,8 @@
-import { connectDB } from './config/db';
-import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
-import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
-
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
@@ -26,16 +14,12 @@ const start = async () => {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
 
-  connectDB();
-
   await natsWrapper.connect(
     process.env.NATS_CLUSTER_ID,
     process.env.NATS_CLIENT_ID,
     process.env.NATS_URL
   );
 
-  new TicketCreatedListener(natsWrapper.client).listen();
-  new TicketUpdatedListener(natsWrapper.client).listen();
   natsWrapper.client.on('close', () => {
     console.log('NATS connection closed!');
     process.exit();
@@ -43,10 +27,6 @@ const start = async () => {
 
   process.on('SIGINT', () => natsWrapper.client.close());
   process.on('SIGTERM', () => natsWrapper.client.close());
-
-  app.listen(PORT, () => {
-    console.log(`App in development is running on ${PORT}`);
-  });
 };
 
 start();
