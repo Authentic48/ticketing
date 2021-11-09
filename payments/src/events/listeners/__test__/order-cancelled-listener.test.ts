75243f579c3ef2578ff231bab0cee964a45aa8ee
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-11-09 13:57:15 +0000

    order cancelled listener  test done

--- payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -1,29 +1,32 @@
-import { OrderCreatedEvent, OrderStatus } from '@authentic48/common';
-import { natsWrapper } from '../../../nats-wrapper';
-import { OrderCreatedListener } from '../order-created-listener';
+import { OrderCancelledEvent, OrderStatus } from '@authentic48/common';
+import { natsWrapper } from '../../../natsWrapper';
+import { OrderCancelledListener } from '../order-cancelled-listener';
 import mongoose from 'mongoose';
 import { Message } from 'node-nats-streaming';
 import { Order } from '../../../models/order';
 
 const setup = async () => {
-  const listener = new OrderCreatedListener(natsWrapper.client);
+  const listener = new OrderCancelledListener(natsWrapper.client);
 
-  const data: OrderCreatedEvent['data'] = {
+  const order = Order.build({
     id: new mongoose.Types.ObjectId().toHexString(),
     version: 0,
     userId: new mongoose.Types.ObjectId().toHexString(),
-    expiresAt: 'bya24187dhsa',
     status: OrderStatus.Created,
+  });
+
+  await order.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: 1,
+    status: OrderStatus.Cancelled,
     ticket: {
-      id: new mongoose.Types.ObjectId().toHexString(),
-      title: 'this is our title',
-      price: 254,
-      userId: new mongoose.Types.ObjectId().toHexString(),
+      id: 'lnbuydagbuvscau',
     },
   };
 
   // @ts-ignore
-
   const msg: Message = {
     ack: jest.fn(),
   };
@@ -31,14 +34,14 @@ const setup = async () => {
   return { listener, data, msg };
 };
 
-it('replicates the order info', async () => {
+it('updates the status of the order', async () => {
   const { listener, data, msg } = await setup();
 
   await listener.onMessage(data, msg);
 
   const order = await Order.findById(data.id);
 
-  expect(order!.status).toEqual(data.status);
+  expect(order!.status).toEqual(OrderStatus.Cancelled);
 });
 
 it('acks the message', async () => {
